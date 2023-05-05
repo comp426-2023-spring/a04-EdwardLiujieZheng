c736fd67722271a38e7cd44848631cb7b104aee1
Edward Liujie Zheng <liujiez@email.unc.edu>
2023-05-05 16:35:10 -0400

    change to import and test

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 // server.js
-const express = require('express');
-const minimist = require('minimist');
-const rpsls = require('./lib/rpsls');
+import express from 'express';
+import bodyParser from 'body-parser';
+import minimist from 'minimist';
+import { rps, rpsls } from './lib/rpsls.js';
+
+// parse argv
+const argv = minimist(process.argv.slice(2));
+console.log(argv);
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
